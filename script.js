94a544e8e103371f32ae173bf816ca77c9dc4e71
mirazh-ds <131595327+mirazh-ds@users.noreply.github.com>
2023-05-18 19:04:40 +0200

    Definisce funzione playRound

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
-function getComputerChoice() {
-    const scelte = ["Sasso", "Carta", "Forbici"];
 
-    const random = scelte[Math.floor(Math.random() * scelte.length)];
+
+
+function playRound() {
+
+
+const playerChoice = prompt("Sasso, Carta, Forbici"); 
+const scelte = ["Rock", "Paper", "Scissors"];
+const computerChoice = scelte[Math.floor(Math.random() * scelte.length)];
+
+const vittoria = "Hai vinto";
+const sconfitta = "Hai perso";
+const pareggio = "Pareggio";
+
+        if (playerChoice == "rock" && computerChoice == "Paper") {
+            return sconfitta;
+        } else if (playerChoice == "rock" && computerChoice == "Scissors") {
+            return vittoria;
+        } else if (playerChoice == "paper" && computerChoice == "Scissors") {
+            return sconfitta; 
+        } else if (playerChoice == "paper" && computerChoice == "Rock") {
+            return vittoria; 
+        } else if (playerChoice == "scissors" && computerChoice == "Paper") {
+            return sconfitta;
+        } else if (playerChoice == "scissors" && computerChoice == "Rock") {
+            return vittoria; 
+        } else {
+            return pareggio;
+        }
     
-    return random;
 }
+
+ 
+
+
+
+
+
+      
