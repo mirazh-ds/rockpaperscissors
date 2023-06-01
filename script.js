257cda0f0b8868b14ebd462b92b383a516852d2d
mirazh-ds <131595327+mirazh-ds@users.noreply.github.com>
2023-06-01 17:42:45 +0200

    completa script gioco

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const risultato = document.getElementById("result");
+const pcPoints = document.getElementById("pcPoints");
+const cpuPoints = document.getElementById("cpuPoints");
+const pcChoice = document.getElementById("pcChoice");
+const cpuChoice = document.getElementById("cpuChoice");
+
+let pcPt = 0;
+let cpuPt = 0;
+
+cpuPoints.textContent = `Punteggio CPU: ${cpuPt}`;
+pcPoints.textContent = `Punteggio giocatore: ${pcPt}`;
+
+pcChoice.textContent = "Scelta giocatore:";
+cpuChoice.textContent = "Scelta CPU:";
 
 const scelte = ["Rock", "Paper", "Scissors"];
 
@@ -15,9 +28,25 @@ rock.addEventListener("click", () => {
     if (computerChoice == "Rock") {
         risultato.textContent = pareggio;
     } else if (computerChoice == "Paper") {
-        risultato.textContent = sconfitta;
+        risultato.textContent = sconfitta;  cpuPt += 1;
     } else {
-        risultato.textContent =  vittoria;    
+        risultato.textContent =  vittoria;   pcPt += 1; 
+    }
+
+    pcPoints.textContent = `Punteggio giocatore: ${pcPt}`;
+    cpuPoints.textContent = `Punteggio CPU: ${cpuPt}`;
+
+    pcChoice.textContent = 'Scelta giocatore: Sasso';
+    cpuChoice.textContent = `Scelta CPU: ${computerChoice}`;
+
+    if (cpuPt == 5) {
+        alert("Vittoria CPU");
+        setTimeout("location.reload(true)");
+    }
+
+    if (pcPt == 5) {
+        alert("Vittoria giocatore");
+        setTimeout("location.reload(true)");
     }
 
 })
@@ -25,11 +54,26 @@ rock.addEventListener("click", () => {
 paper.addEventListener("click", () => {
     const computerChoice = scelte[Math.floor(Math.random() * scelte.length)];
     if (computerChoice == "Rock") {
-        risultato.textContent = vittoria;
+        risultato.textContent = vittoria; pcPt += 1; 
     } else if (computerChoice == "Paper") {
         risultato.textContent = pareggio;
     } else {
-        risultato.textContent = sconfitta;    
+        risultato.textContent = sconfitta;   cpuPt += 1; 
+    }
+    pcPoints.textContent = `Punteggio giocatore: ${pcPt}`;
+    cpuPoints.textContent = `Punteggio CPU: ${cpuPt}`;
+
+    pcChoice.textContent = 'Scelta giocatore: Carta';
+    cpuChoice.textContent = `Scelta CPU: ${computerChoice}`;
+
+    if (cpuPt == 5) {
+        alert("Vittoria CPU");
+        setTimeout("location.reload(true)");
+    }
+
+    if (pcPt == 5) {
+        alert("Vittoria giocatore");
+        setTimeout("location.reload(true)");
     }
 
 })
@@ -37,14 +81,30 @@ paper.addEventListener("click", () => {
 scissors.addEventListener("click", () => {
     const computerChoice = scelte[Math.floor(Math.random() * scelte.length)];
     if (computerChoice == "Rock") {
-        risultato.textContent = sconfitta;
+        risultato.textContent = sconfitta;  cpuPt += 1;
     } else if (computerChoice == "Paper") {
-        risultato.textContent =  vittoria;
+        risultato.textContent =  vittoria; pcPt += 1; 
     } else { 
         risultato.textContent = pareggio;    
     }
+    pcPoints.textContent = `Punteggio giocatore: ${pcPt}`;
+    cpuPoints.textContent = `Punteggio CPU: ${cpuPt}`;
 
+    pcChoice.textContent = 'Scelta giocatore: Forbici';
+    cpuChoice.textContent = `Scelta CPU: ${computerChoice}`;
+
+    if (cpuPt == 5) {
+        alert("Vittoria CPU");
+        setTimeout("location.reload(true)");
+    }
+
+    if (pcPt == 5) {
+        alert("Vittoria giocatore");
+        setTimeout("location.reload(true)");
+    }
 })
+
+
 
 
 // let playerChoice
@@ -78,49 +138,49 @@ scissors.addEventListener("click", () => {
     
 // }
 
-// function playMatch() {
+function playMatch() {
 
-//     let giocati = 1;
-//     let ptPlayer = 0;
-//     let ptCpu = 0
-//     let pareggi = 0;
+    let giocati = 1;
+    let ptPlayer = 0;
+    let ptCpu = 0
+    let pareggi = 0;
 
-        // for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 5; i++) {
             
-        //     console.log(`Round numero: ${giocati}`)
+            console.log(`Round numero: ${giocati}`)
 
-        //     const playerChoice = prompt("Sasso, Carta, Forbici").toLowerCase(); 
-        //     const scelte = ["Rock", "Paper", "Scissors"];
-        //     const computerChoice = scelte[Math.floor(Math.random() * scelte.length)];
+            const playerChoice = prompt("Sasso, Carta, Forbici").toLowerCase(); 
+            const scelte = ["Rock", "Paper", "Scissors"];
+            const computerChoice = scelte[Math.floor(Math.random() * scelte.length)];
     
-        //     if (playerChoice == "rock" && computerChoice == "Paper") {
-        //         ptCpu++;
-        //     } else if (playerChoice == "rock" && computerChoice == "Scissors") {
-        //         ptPlayer++;
-        //     } else if (playerChoice == "paper" && computerChoice == "Scissors") {
-        //         ptCpu++; 
-        //     } else if (playerChoice == "paper" && computerChoice == "Rock") {
-        //         ptPlayer++; 
-        //     } else if (playerChoice == "scissors" && computerChoice == "Paper") {
-        //         ptCpu++;
-        //     } else if (playerChoice == "scissors" && computerChoice == "Rock") {
-        //         ptPlayer++; 
-        //     } else {
-        //         pareggi++;
-        //     }
+            if (playerChoice == "rock" && computerChoice == "Paper") {
+                ptCpu++;
+            } else if (playerChoice == "rock" && computerChoice == "Scissors") {
+                ptPlayer++;
+            } else if (playerChoice == "paper" && computerChoice == "Scissors") {
+                ptCpu++; 
+            } else if (playerChoice == "paper" && computerChoice == "Rock") {
+                ptPlayer++; 
+            } else if (playerChoice == "scissors" && computerChoice == "Paper") {
+                ptCpu++;
+            } else if (playerChoice == "scissors" && computerChoice == "Rock") {
+                ptPlayer++; 
+            } else {
+                pareggi++;
+            }
             
-        //     giocati++;
+            giocati++;
 
-//             console.log(`Player choice: ${playerChoice}`);
-//             console.log(`Computer choice: ${computerChoice}`);
+            console.log(`Player choice: ${playerChoice}`);
+            console.log(`Computer choice: ${computerChoice}`);
 
             
-//             console.log(`Player: ${ptPlayer}; Computer: ${ptCpu}; Ties: ${pareggi}.`)
+            console.log(`Player: ${ptPlayer}; Computer: ${ptCpu}; Ties: ${pareggi}.`)
             
-//         }
+        }
 
     
-//     }
+    }
 
 
 
